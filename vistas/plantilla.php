7f4a99bc33127d1c2fbc484921f9b103ca4e76ab
alojoarboleda2003 <alonsoarboleda17@gmail.com>
2025-04-07 12:14:22 -0500

    diseñando solicitudes

--- vistas/plantilla.php
+++ vistas/plantilla.php
@@ -7,7 +7,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Hermes FRANCO</title>
+  <title>Hermes ADSO</title>
 
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
